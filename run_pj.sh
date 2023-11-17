89996a46921a3b8264782bd1216195609d27a584
Shirley_Shi <shirley_shi@epamcom>
2023-11-17 10:26:20 +0800

    pytest first version

--- run_pj.sh
+++ run_pj.sh
@@ -4,4 +4,4 @@
 chmod 755  /usr/bin/pytest
 chmod 755 /var/jenkins_home/.local/lib/python3.9/site-packages/pytest
 cd /var/jenkins_home/.local/lib/python3.9/site-packages/pytest
-/var/jenkins_home/.local/lib/python3.9/site-packages/pytest test_tables.py --html=report.html
+/usr/bin/pytest test_tables.py --html=report.html
