6a27f96f555a7edaca91bdac0494cfe80bce5e6a
Shirley_Shi <shirley_shi@epamcom>
2023-11-17 10:06:04 +0800

    pytest first version

--- run_pj.sh
+++ run_pj.sh
@@ -1,4 +1,5 @@
 
 #!/bin/bash
-pip3 install pytest
-pytest --html=report.html
+#pip3 install pytest
+#cd /var/jenkins_home/.local/lib/python3.9/site-packages/pytest
+python -m pytest --html=report.html
