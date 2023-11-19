741f421ccc5ed914f808ed6c15b847286657f63c
Shirley_Shi <shirley_shi@epamcom>
2023-11-19 17:00:26 +0800

    pytest first version

--- run_pj.sh
+++ run_pj.sh
@@ -3,6 +3,7 @@
 #pip3 install pytest
 #pip3 install pytest-html
 #pip3 install pyodbc
+pip3 install pymssql
 chmod 755 /usr/bin/pytest
 chmod 755 test_tables.py
 python3 -m pytest --html=report.html
